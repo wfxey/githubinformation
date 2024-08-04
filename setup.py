faa2e7c03edaaefa8b242c40240922db374ed285
wfxey <158351052+wfxey@users.noreply.github.com>
2024-08-04 16:31:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     license='MIT',
     description='A tool that lets you check your GitHub Project for updates.',
     author='wfxey',
-    author_email='projectsdi02@gmail.com',
+    author_email='di.projects.help@gmail.com',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/wfxey/githubinformation',
